5587227e76529daa07a76442b12d3805e7a67117
tabrezdn1 <tabrezdn1@gmail.com>
2020-11-03 19:57:15 +0530

    Add orders component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,67 @@
 <template>
-  <div>
-    <Order/>
+  <div class="app">
+    <div class="d-flex justify-content-center"><h1>Pizza Orders</h1></div>
+    <div class="d-flex flex-column bd-highlight mb-3">
+      <Order :orders="orders" />
+    </div>
   </div>
 </template>
 
 <script>
-import Order from './components/Order.vue'
+import Order from "./components/Order.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Order
-  }
-}
+    Order,
+  },
+  data() {
+    return {
+      orders: [
+        {
+          id: 1,
+          customerName: "Shaik Tabrez",
+          orderItems: [
+            { name: "One", cost: 10 },
+            { name: "Two", cost: 20 },
+          ],
+          status: 0,
+        },
+        {
+          id: 2,
+          customerName: "Jeeva",
+          orderItems: [
+            { name: "One", cost: 10 },
+            { name: "Two", cost: 20 },
+          ],
+          status: -1,
+        },
+        {
+          id: 3,
+          customerName: "Alex",
+          orderItems: [
+            { name: "One", cost: 10 },
+            { name: "Two", cost: 20 },
+          ],
+          status: 1,
+        },
+        {
+          id: 4,
+          customerName: "John",
+          orderItems: [
+            { name: "One", cost: 10 },
+            { name: "Two", cost: 20 },
+          ],
+          status: 0,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
+.app{
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+}
 </style>
--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,11 +1,113 @@
 <template>
-  <div>
-    <p>I am order component</p>
+  <div class="p-2 bd-highlight">
+    <b-card-group
+      class="p-2 py-2"
+      deck
+      v-for="(order, orderIndex) in dataOrders"
+      :key="orderIndex"
+    >
+      <b-card
+        :header="'Customer : ' + order.customerName"
+        :class="getDynamicClass(order.status)"
+      >
+        <b-list-group>
+          <b-list-group-item
+            >Items ordered
+            <b-badge variant="primary" pill>{{
+              order.orderItems.length
+            }}</b-badge></b-list-group-item
+          >
+          <b-list-group-item
+            href="#"
+            v-for="(orderItem, orderItemIndex) in order.orderItems"
+            :key="orderItemIndex"
+            >{{ orderItem.name }}</b-list-group-item
+          >
+          <b-list-group-item
+            >Sub Total:
+            <strong> {{ getSubtotal(order.id) }} </strong></b-list-group-item
+          >
+        </b-list-group>
+        <div>
+          <h3>Status</h3>
+          <template v-if="order.status == -1">
+            Received
+            <b-icon icon="three-dots" animation="throb" font-scale="2"></b-icon>
+          </template>
+          <template v-if="order.status == 0">
+            Preparing
+            <b-icon
+              icon="arrow-clockwise"
+              animation="spin-pulse"
+              font-scale="2"
+            ></b-icon>
+          </template>
+          <template v-if="order.status == 1">
+            Ready to serve
+            <b-icon icon="check-all" animation="throb" font-scale="2"></b-icon>
+          </template>
+
+          <button
+            v-if="order.status == -1 || order.status == 0"
+            class="btn btn-secondary btn-lg float-right"
+            @click="updateStatus(order.id)"
+          >
+            Change Status
+          </button>
+        </div>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Order'
-}
+  name: "Order",
+  props: {
+    orders: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      dataOrders: [],
+    };
+  },
+  mounted() {
+    this.dataOrders = this.orders;
+  },
+  methods: {
+    getSubtotal(id) {
+      let order = this.dataOrders.find((item) => item.id == id);
+      let items = order.orderItems;
+      let subTotal = 0;
+
+      for (let i = 0; i < items.length; i++) {
+        subTotal += items[i]["cost"];
+      }
+      return subTotal;
+    },
+    updateStatus(id) {
+      let order = this.dataOrders.find((item) => item.id == id);
+      if (order.status < 1) {
+        for (let i = 0; i < this.dataOrders.length; i++) {
+          if (this.dataOrders[i]["id"] == id) {
+            this.dataOrders[i]["status"] += 1;
+            return;
+          }
+        }
+      }
+    },
+    getDynamicClass(status) {
+      if (status == 1) {
+        return "bg-success";
+      } else if (status == 0) {
+        return "bg-info";
+      } else {
+        return "bg-warning";
+      }
+    },
+  },
+};
 </script>
